8548b61e6cba15c691b89d71302e022f27362010
Siddharth Panigrahi <siddharthpanigrahi@Siddharths-MacBook-Pro.local>
2023-08-10 16:27:40 -0700

    rill project files

---
+++ models/Payments_model.sql
@@ -0,0 +1,2 @@
+select AMOUNT as amount, "CHECK DATE" as check_date, "VOUCHER NUMBER" as voucher_number, "VENDOR NAME" AS vendor_name, "DEPARTMENT NAME" as department_name, CASHED AS cashed
+from Payments
