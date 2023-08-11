f048ca307012f1850120d2f3c8b57566b3249508
Siddharth Panigrahi <siddharthpanigrahi@Siddharths-MacBook-Pro.local>
2023-08-11 10:22:03 -0700

    date formatting changes in dash

--- models/Payments_model.sql
+++ models/Payments_model.sql
@@ -5,7 +5,7 @@ CASE
   WHEN LENGTH(check_date) = 4 THEN check_date
 ELSE null
 END AS check_year,
-strptime (check_year, '%Y') as check_year_new, 
+cast(strptime (check_year, '%Y') as DATE) as check_year_new, 
 "VOUCHER NUMBER" as voucher_number,
  "VENDOR NAME" AS vendor_name, 
 "DEPARTMENT NAME" as department_name, 
