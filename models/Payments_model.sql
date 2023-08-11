92c1a731ebedf8d910b055796ebefacb49c0f7ad
Siddharth Panigrahi <siddharthpanigrahi@Siddharths-MacBook-Pro.local>
2023-08-10 17:06:22 -0700

    date formatting and time series added

--- models/Payments_model.sql
+++ models/Payments_model.sql
@@ -1,2 +1,13 @@
-select AMOUNT as amount, "CHECK DATE" as check_date, "VOUCHER NUMBER" as voucher_number, "VENDOR NAME" AS vendor_name, "DEPARTMENT NAME" as department_name, CASHED AS cashed
+select AMOUNT as amount, 
+"CHECK DATE" AS check_date,
+CASE 
+  WHEN LENGTH(check_date) = 10 THEN SUBSTRING(check_date, 7, 4)
+  WHEN LENGTH(check_date) = 4 THEN check_date
+ELSE null
+END AS check_year,
+strptime (check_year, '%Y') as check_year_new, 
+"VOUCHER NUMBER" as voucher_number,
+ "VENDOR NAME" AS vendor_name, 
+"DEPARTMENT NAME" as department_name, 
+CASHED AS cashed
 from Payments
